e6d99f5d51b4e7edc639a057d398f8be331f5a42
pressUp2Start <114031379+pressUp2Start@users.noreply.github.com>
2023-12-11 15:00:52 -0600

    Created item and object classes

---
+++ Item.cs
@@ -0,0 +1,28 @@
+﻿namespace ModernSoftwareDevelopmentAssignment5
+{
+    public class Item
+    {
+        public Item(int id, int num, decimal price)
+        {
+            this.id = id;
+            ammount = num;
+            unitPrice = price;
+        }
+        
+        int id;
+        int ammount;
+        decimal unitPrice;
+
+        public int getID() { return id; }
+
+        public decimal getPrice()
+        {
+            return ammount * unitPrice;
+        }
+
+        public void addAmount(int num)
+        {
+            ammount += num;
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,62 @@
+﻿using NuGet.Protocol.Plugins;
+
+namespace ModernSoftwareDevelopmentAssignment5
+{
+    public class Order
+    {
+        public Order() { }
+
+        List<Item> itemList;
+
+        public void addItem(int id, int num)
+        {
+            //Attach price to database
+            decimal price = 0;
+            bool added = false;
+            if(itemList.Count > 0)
+            {
+                foreach(Item i in itemList) {
+                    if (i.getID() == id)
+                    {
+                        i.addAmount(num);
+                        added = true;
+                    }
+                }
+            }
+            if(added)
+            {
+                Item temp = new Item(id, num, price);
+                itemList.Add(temp);
+            }
+            
+        }
+
+        public void deleteItem(int id)
+        {
+            if(itemList.Count > 0)
+            {
+                foreach (Item i in itemList)
+                {
+                    if (i.getID() == id)
+                    {
+                        itemList.Remove(i);
+                    }
+                }
+            }
+        }
+
+        public decimal getTotalPrice()
+        {
+            if(itemList.Count > 0)
+            {
+                decimal total = 0;
+                foreach (Item i in itemList)
+                {
+                    total += i.getPrice();
+                }
+                return total;
+            }
+            return 0;
+        }
+    }
+}
